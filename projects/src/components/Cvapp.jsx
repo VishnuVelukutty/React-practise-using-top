c6b93c635ee82c1eff83bbd82f98ac345d755291
vishnuvelukutty <vishnuvelukutty26@gmail.com>
2024-01-05 17:39:48 +0530

    added new

--- projects/src/components/Cvapp.jsx
+++ projects/src/components/Cvapp.jsx
@@ -16,8 +16,8 @@ function Button(props) {
 
 export default function Cvapp() {
 
-    const [fname, setFname] = useState('');
-    const [sName, setSname] = useState('');
+    const [fName, setFname] = useState('');
+    const [sName, setSname] = useState();
 
 
     return (
@@ -27,26 +27,110 @@ export default function Cvapp() {
 
             <hr />
 
-            <div><Input type='text' value={fname} placeholder='Name' onChange={(event) => {
+            <div><p>basic details</p></div>
+            <div><input type='text' placeholder='Name' onChange={(event) => {
                 setFname(event.target.value)
             }
-            }></Input></div>
+            }></input></div>
+
+
+            <div><input type='text' placeholder='Surname' onChange={(event) => {
+                setSname(event.target.value)
+            }
+            }></input></div>
+
+
+
+            <div><input type='text' placeholder='Email' onChange={(event) => {
+                setFname(event.target.value)
+            }
+            }></input></div>
+
+
+            <div><input type='text' placeholder='Phone' onChange={(event) => {
+                setFname(event.target.value)
+            }
+            }></input></div>
+
+            <hr />
+
+            <div><p>Education details</p></div>
+
+
+            <div>
+                <input type='text' placeholder='School Name'></input>
+            </div>
+
+            <div>
+                <input type='text' placeholder='Title of Study'></input>
+            </div>
+
+            <div>
+                <input  type='text'placeholder='Year'></input>
+            </div>
+
+
+            <div><p>Experience details</p></div>
+
+            <div>
+                <input type='text' placeholder='company name'></input>
+            </div>
+
+            <div>
+                <input type='text' placeholder='position title'></input>
+            </div>
+
+            <div>
+                <input type='text' placeholder='responsibilities'></input>
+            </div>
+
+
+            <div>
+                <input type='text' placeholder='from'></input>
+            </div>
+            <div>
+                <input type='text' placeholder='to'></input>
+            </div>
+
+
+
 
             {/**
              * setting value will set it permanently
+             * not working for custom component i.e <Input > check why 
              */}
 
-            <div><Input type='text' value={sName} placeholder='Surname' onChange={(event)=>{
-                setSname(event.target.value)
-            }}></Input></div>
             <br />
             <hr />
 
             <div><h1>Displayer</h1></div>
 
-            <div><span>NAME : </span><br /><h1>{fname}</h1></div>
+
+            <div><p>Basic Details</p></div>
+
+            <div><span>NAME : </span><br /><h1>{fName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+
+            <div><p>Education Details</p></div>
 
             <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+
+
+            <div><p>Experience Details</p></div>
+
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+            <div><span>SURNAME : </span><br /><h1>{sName}</h1></div>
+
+
+
+
 
 
 
